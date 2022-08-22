9e98f8a7800d9e695107c0adfe4f23fa9c80c44b
nbrown225 <chbrown225@yahoo.com>
2022-08-21 19:45:35 -0700

    added main page. will change colors

--- meal-planner/src/components/Today/today.js
+++ meal-planner/src/components/Today/today.js
@@ -2,24 +2,21 @@ import React from 'react';
 
 function Today(){
     return(
-        
         <div className='card col s5' id='today-card'>
-                    <div >
-                    <div className='card-title center' id='card-title'>DATE</div>
-                    <div className='card-content'>
-                        <p className='meal-today'>
-                            Today's planned meal
-                        </p> 
-                        <p className='meal-search'>
-                            don't have a meal planned? find one!
-                        </p>
-                        <placeholder>button or input to search</placeholder>
-                    </div>
+            <div >
+                <div className='card-title center' id='today-title'>DATE</div>
+                <div className='card-content' id='today-content'>
+                    <p className='meal-today center' id='today-p'>
+                        Today's planned meal:
+                    </p> 
+                    <a href='' className=''>see recipe</a>
+                    <p className='meal-search' id='go-to-search'>
+                        don't have a meal planned? find one!
+                    </p>
+                    <a href='' className=''>plan meal</a>
                 </div>
-
-
-                </div>
-
+            </div>
+        </div>
 
                 
 
