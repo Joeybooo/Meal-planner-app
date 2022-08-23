575d529546971cb01f0f3c90293b2429d12ab782
Joey Byrd <joeybyrd9999@gmail.com>
2022-08-23 10:20:50 -0700

    added react header

---
+++ meal-planner/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <nav className='navbar'>
+                <div className='navbar-branch'>
+                    <a className='navbar-item' rel='noreferrer' target="_blank" href=" ">
+                        <span>Timely Meals</span>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
