c0ca6d8654d98d83285227eabc1c414e945a78ed
Joey Byrd <joeybyrd9999@gmail.com>
2022-08-23 10:31:53 -0700

    added navigation

---
+++ meal-planner/src/components/StartingPage/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Nav from "../Nav/nav";
+
+function Navigation(props) {
+    const tabs = [ "About", "Login/Sign-up"];
+    return (
+        <div className="tabs is-centered">
+            <ul className="nav nav-tabs">
+                {tabs.map((tab) => (
+                    <li className={ props.currentPage === tab ? "nav-item is-active" : "nav-item"} key={tab}>
+                        <a href={"#" + tab.toLowerCase()} onClick={() => props.handlePageChange(tab)} className={props.currentPage === tab ? "nav-link active" : "nav-link"} >
+                            {tab}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Navigation;
