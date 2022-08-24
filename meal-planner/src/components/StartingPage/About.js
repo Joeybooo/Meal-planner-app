9e8a09941e19e38d73f4bb2afad18702e0b1945d
unknown <nardissoloab@gmail.com>
2022-08-24 14:55:47 -0500

    logic

--- meal-planner/src/components/StartingPage/About.js
+++ meal-planner/src/components/StartingPage/About.js
@@ -9,13 +9,13 @@ function About() {
         <div>
            <p className="content">
                 With the busy lives we live we struggle to find time to cook and find recipes. No longer will it be a struggle once you start using Timely Meals.
-                We aim to give users the best experience possible with easy to plann options.  
+                We aim to give users the best experience possible with easy to plan options.  
            </p>
            <div>
                 <article>
                     <div>
                         <h3>Home Page</h3>
-                        <p> This will be your home page. It will show you your daily recipes you choosen for the day. With its design you can be ready for any meal you have planned</p>
+                        <p> This will be your home page. It will show you your daily recipes you chosen for the day. With its design you can be ready for any meal you have planned</p>
                     </div>
                     <img className="about-img" src={fillerImage} alt="Home Page"/>
                 </article>
@@ -30,10 +30,10 @@ function About() {
 
                 <article>
                     <div>
-                        <h3>Calander</h3>
-                        <p> With your calander you will be able to see what days you have meals planned for. You can even click the day to see what you have planned. You can remove recipes from days of the calander</p>
+                        <h3>Calender</h3>
+                        <p> With your calender you will be able to see what days you have meals planned for. You can even click the day to see what you have planned. You can remove recipes from days of the calander</p>
                     </div>
-                    <img className="about-img" src={fillerImage} alt="Calander"/>
+                    <img className="about-img" src={fillerImage} alt="Calender"/>
                 </article>
 
                 <article>
@@ -41,7 +41,7 @@ function About() {
                         <h3>Create Your Own Recipe</h3>
                         <p> With the ability to create your own recipes you can save them for yourself or you can post them for other users to find.</p>
                     </div>
-                    <img className="about-img" src={fillerImage} alt="Creat Recipes"/>
+                    <img className="about-img" src={fillerImage} alt="Create Recipes"/>
                 </article>
            </div>
         </div>
